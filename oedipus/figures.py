b91d333edb0c198576ba9f531e94db1eff597959
Paul Boone <narcissus.pong@gmail.com>
2018-09-13 15:42:39 -0400

    colors for projector

--- oedipus/figures.py
+++ oedipus/figures.py
@@ -23,33 +23,33 @@ def delaunay_figure(boxes, convergence_bins, output_path, triang=None, children=
     ax.set_xticks(np.array(range(0,convergence_bins + 1))/convergence_bins)
     ax.set_yticks(np.array(range(0,convergence_bins + 1))/convergence_bins)
     ax.tick_params(labelbottom=False, labelleft=False)
-    ax.grid(linestyle='-', color='0.7', zorder=0)
+    ax.grid(linestyle='-', color='0.5', zorder=0)
 
     dbin = 1.0 / convergence_bins
     bin_rects = [Rectangle((b[0] * dbin, b[1] * dbin), dbin, dbin) for b in bins]
-    pc = PatchCollection(bin_rects, facecolor='0.85')
+    pc = PatchCollection(bin_rects, facecolor='0.75')
     ax.add_collection(pc)
 
     new_bin_rects = [Rectangle((b[0] * dbin, b[1] * dbin), dbin, dbin) for b in new_bins]
-    pc2 = PatchCollection(new_bin_rects, facecolor='#B1E0E0')
+    pc2 = PatchCollection(new_bin_rects, facecolor='#82b7b7')
     ax.add_collection(pc2)
 
     # plot all points as triangulation
-    ax.triplot(boxes[:,3], boxes[:,4], triang.simplices.copy(), 'b-', lw=1)
+    ax.triplot(boxes[:,3], boxes[:,4], triang.simplices.copy(), color='#78a7cc', lw=1)
 
     # plot hull and labels
-    ax.plot(hull_points[:,3], hull_points[:,4], color='blue', marker='o', linestyle='None', zorder=10)
+    ax.plot(hull_points[:,3], hull_points[:,4], color='#78a7cc', marker='o', linestyle='None', zorder=10)
     # for p in hull_points:
     #     ax.annotate(i, (p[0], p[1] - 0.01), zorder=30, ha="center", va="top", size=9)
 
     # plot children
-    ax.plot(children[:,3], children[:,4], color='yellow', marker='o', linestyle='None', zorder=12)
+    ax.plot(children[:,3], children[:,4], color='#81ff6b', marker='o', linestyle='None', zorder=12)
 
     # plot chosen parents with proportional circles and label
     if len(parents) > 0:
         parent_counter = Counter([tuple(x) for x in parents]) #need tuples because they are hashable
         unique_parents = np.array([[x[3], x[4], num] for x, num in parent_counter.items()])
-        ax.scatter(unique_parents[:,0], unique_parents[:,1], s=40*unique_parents[:,2], color='orange', marker='o', linestyle='None', zorder=15)
+        ax.scatter(unique_parents[:,0], unique_parents[:,1], s=40*unique_parents[:,2], color='#ffbe6b', marker='o', linestyle='None', zorder=15)
         for p in unique_parents:
             x, y, parent_count = p
             if parent_count > 5:
