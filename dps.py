2010279463a8e975e819aa87e27264860637b75e
Paul Boone <narcissus.pong@gmail.com>
2018-12-14 10:14:15 -0500

    remove old erroneous help comment

--- dps.py
+++ dps.py
@@ -7,14 +7,6 @@ from oedipus.oedipus import oedipus
 @click.command()
 @click.argument('config_path',type=click.Path())
 def dps(config_path):
-    """Start process to manage run.
-
-    Args:
-        run_id (str): identification string for run.
-
-    Runs OEDIPUS-method in one process.
-
-    """
     oedipus(config_path)
 
 if __name__ == '__main__':
