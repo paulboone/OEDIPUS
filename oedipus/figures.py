6e298a2bee5a0457f89800e762dc2a57058a441e
Paul Boone <narcissus.pong@gmail.com>
2019-02-27 13:38:15 -0500

    incorporate htsohm updates to figure code, including perturbation arrow display and axes with bounds that are not 0.0-1.0 (this latter should be unnecessary here)

--- oedipus/figures.py
+++ oedipus/figures.py
@@ -7,7 +7,8 @@ import numpy as np
 from scipy.spatial import Delaunay
 
 def delaunay_figure(box_r, convergence_bins, output_path, triang=None, children=[], parents=[],
-                    bins=[], new_bins=[], title="", patches=None):
+                    bins=[], new_bins=[], title="", patches=None, prop1range=(0.0,1.0), prop2range=(0.0,1.0),
+                    perturbation_methods=None):
 
     if not triang:
         triang = Delaunay(box_r)
@@ -18,19 +19,21 @@ def delaunay_figure(box_r, convergence_bins, output_path, triang=None, children=
     # plot visualization
     fig = plt.figure(figsize=(12,12), tight_layout=True)
     ax = fig.add_subplot(1, 1, 1)
-    ax.set_ylim(0.0, 1.0)
-    ax.set_xlim(0.0, 1.0)
-    ax.set_xticks(np.array(range(0,convergence_bins + 1))/convergence_bins)
-    ax.set_yticks(np.array(range(0,convergence_bins + 1))/convergence_bins)
+    ax.set_xlim(prop1range[0], prop1range[1])
+    ax.set_ylim(prop2range[0], prop2range[1])
+
+    ax.set_xticks(prop1range[1] * np.array(range(0,convergence_bins + 1))/convergence_bins)
+    ax.set_yticks(prop2range[1] * np.array(range(0,convergence_bins + 1))/convergence_bins)
     ax.tick_params(labelbottom=False, labelleft=False)
     ax.grid(linestyle='-', color='0.5', zorder=0)
 
-    dbin = 1.0 / convergence_bins
-    bin_rects = [Rectangle((b[0] * dbin, b[1] * dbin), dbin, dbin) for b in bins]
+    dbinx = prop1range[1] / convergence_bins
+    dbiny = prop2range[1] / convergence_bins
+    bin_rects = [Rectangle((b[0] * dbinx, b[1] * dbiny), dbinx, dbiny) for b in bins]
     pc = PatchCollection(bin_rects, facecolor='0.75')
     ax.add_collection(pc)
 
-    new_bin_rects = [Rectangle((b[0] * dbin, b[1] * dbin), dbin, dbin) for b in new_bins]
+    new_bin_rects = [Rectangle((b[0] * dbinx, b[1] * dbiny), dbinx, dbiny) for b in new_bins]
     pc2 = PatchCollection(new_bin_rects, facecolor='#82b7b7')
     ax.add_collection(pc2)
 
@@ -58,6 +61,15 @@ def delaunay_figure(box_r, convergence_bins, output_path, triang=None, children=
     if patches == "donut":
         ax.add_patch(Circle((0.5, 0.5), 0.125, fill=False, linestyle="dashed", linewidth=1, zorder=50))
         ax.add_patch(Circle((0.5, 0.5), 0.375, fill=False, linestyle="dashed", linewidth=2, zorder=50))
+
+    if perturbation_methods:
+        p_cmap = {"lattice": "w", "density": "black", "atom_types": "#0FA3B1", "atom_sites": "#F77936", }
+        arrows = [ConnectionPatch(parents[i], children[i], "data", arrowstyle="-|>", \
+                    shrinkA=5, shrinkB=5, mutation_scale=20, fc=p_cmap[perturbation_methods[i]], \
+                    linestyle="--", zorder=40) \
+                    for i in range(len(children))]
+        for a in arrows:
+            ax.add_patch(a)
 
     ax.set_title(title)
     fig.savefig(output_path)
